de9bea570861faad988870dd6b18f48a93e0a871
João Tavares <joaotavares@MacBook-Pro-de-Joao.local>
2023-08-22 16:56:14 -0300

    added n()

--- src/index.js
+++ src/index.js
@@ -579,7 +579,7 @@ class PepperSpray {
     let _letters = [];
     let result = {
       word,
-      wide: cangjie['value'],
+      wide: cangjie['ten'],
       web: []
     };
     
@@ -595,19 +595,19 @@ class PepperSpray {
     if (n.length >= 2) {
       switch(n[1]) {
         case 'value':
-          result.wide = cangjie['value'];
+          result.wide = cangjie['wood'];
           break;
 
         case 'function':
-          result.wide = cangjie['value'];
+          result.wide = cangjie['twenty'];
           break;
 
         case 'behavior':
-          result.wide = cangjie['value'];
+          result.wide = cangjie['bow'];
           break;
 
         case 'result':
-          result.wide = cangjie['value'];
+          result.wide = cangjie['fire'];
           break;
       }
     }

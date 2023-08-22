193dca6bc777382a38b0f92735ccc7f872ffb407
João Tavares <joaotavares@MacBook-Pro-de-Joao.local>
2023-08-22 15:53:47 -0300

    reduced repeated code

--- src/index.js
+++ src/index.js
@@ -20,40 +20,13 @@ class PepperSpray {
     return 'consonants';
   }
 
-  parse() {
-    this.phrase = this.phrase.toLowerCase().split(' ');
-  }
-
-  translate() {
-    this.phrase.forEach((word) => {
-      let w = Object.values(word);
-      let f = w[0];
-      
-      this[f].call(this, word, w);
-    });
-  }
-
-  a(word, w) {
-    let _letters = [];
-    let result = {
-      word,
-      wide: cangjie['corpse'],
-      web: []
-    };
-    
-    w.forEach((l) => {
-      result.web.push({
-        l,
-        v: letters[l]
-      });
-    });
-
+  getValues(r) {
     // Get result for wide
     let syllable = false;
     let syllable_letters = [];
     let n = [];
 
-    result.web.forEach((_l, i) => {
+    r.web.forEach((_l, i) => {
       let letter_type_group = this.getType(_l.l);
 
       if (!syllable_letters.length) {
@@ -101,6 +74,39 @@ class PepperSpray {
     });
 
     if (this.debug) console.log(n);
+
+    return n;
+  }
+
+  parse() {
+    this.phrase = this.phrase.toLowerCase().split(' ');
+  }
+
+  translate() {
+    this.phrase.forEach((word) => {
+      let w = Object.values(word);
+      let f = w[0];
+      
+      this[f].call(this, word, w);
+    });
+  }
+
+  a(word, w) {
+    let _letters = [];
+    let result = {
+      word,
+      wide: cangjie['corpse'],
+      web: []
+    };
+    
+    w.forEach((l) => {
+      result.web.push({
+        l,
+        v: letters[l]
+      });
+    });
+    
+    let n = this.getValues(result);
 
     if (n.length >= 2) {
       switch(n[1]) {
@@ -136,59 +142,7 @@ class PepperSpray {
       });
     });
 
-    // Get result for wide
-    let syllable = false;
-    let syllable_letters = [];
-    let n = [];
-
-    result.web.forEach((_l, i) => {
-      let letter_type_group = this.getType(_l.l);
-
-      if (!syllable_letters.length) {
-        syllable = true;
-        syllable_letters.push(_l.l);
-
-        n.push(_l.v);
-      } else {
-        let not_a_syllable_of = (letter) => { !letters_knife[letter]['waits_for'][letter_type_group].includes(_l.l) };
-        let not_a_syllable_after_of = (letter) => { return letters_knife[letter]['waits_for']['_after'][_l.l] === undefined };
-        let syllable_after_of = (letter) => { return letters_knife[letter]['waits_for']['_after'][_l.l] };
-
-        if (syllable_letters.length === 1 && not_a_syllable_of(syllable_letters[0])) {
-          syllable_letters = [_l.l];
-
-          n.push(_l.v);
-        } else if (syllable_letters.length === 2 && not_a_syllable_after_of(syllable_letters[1])) {
-          syllable_letters = [_l.l];
-
-          n.push(_l.v);
-        } else if (syllable_letters.length >= 3 && syllable_after_of(syllable_letters[1])) { //del
-          let a = syllable_letters[0];
-          let b = syllable_letters[1];
-          let c = syllable_letters[2];
-          let d = letters_knife[b]['waits_for']['_after'][c];
-
-          let syllable_word = '';
-          let size = syllable_letters.length;
-          let word = a + b + c;
-          let checksum = false;
-
-          for (let i = 0; i < size; i++) syllable_word += syllable_letters[i];
-
-          for (let i = 0; i < d.length; i++) if (!checksum && (word + d[i]).includes(syllable_word)) checksum = true;
-
-          if (!checksum) {
-            syllable_letters = [_l.l];
-
-            n.push(_l.v);
-          }
-        } else {
-          syllable_letters.push(_l.l);
-        }
-      }
-    });
-
-    if (this.debug) console.log(n);
+    let n = this.getValues(result);
 
     if (n.length >= 2) {
       switch(n[1]) {
@@ -224,59 +178,7 @@ class PepperSpray {
       });
     });
 
-    // Get result for wide
-    let syllable = false;
-    let syllable_letters = [];
-    let n = [];
-
-    result.web.forEach((_l, i) => {
-      let letter_type_group = this.getType(_l.l);
-
-      if (!syllable_letters.length) {
-        syllable = true;
-        syllable_letters.push(_l.l);
-
-        n.push(_l.v);
-      } else {
-        let not_a_syllable_of = (letter) => { !letters_knife[letter]['waits_for'][letter_type_group].includes(_l.l) };
-        let not_a_syllable_after_of = (letter) => { return letters_knife[letter]['waits_for']['_after'][_l.l] === undefined };
-        let syllable_after_of = (letter) => { return letters_knife[letter]['waits_for']['_after'][_l.l] };
-
-        if (syllable_letters.length === 1 && not_a_syllable_of(syllable_letters[0])) {
-          syllable_letters = [_l.l];
-
-          n.push(_l.v);
-        } else if (syllable_letters.length === 2 && not_a_syllable_after_of(syllable_letters[1])) {
-          syllable_letters = [_l.l];
-
-          n.push(_l.v);
-        } else if (syllable_letters.length >= 3 && syllable_after_of(syllable_letters[1])) { //del
-          let a = syllable_letters[0];
-          let b = syllable_letters[1];
-          let c = syllable_letters[2];
-          let d = letters_knife[b]['waits_for']['_after'][c];
-
-          let syllable_word = '';
-          let size = syllable_letters.length;
-          let word = a + b + c;
-          let checksum = false;
-
-          for (let i = 0; i < size; i++) syllable_word += syllable_letters[i];
-
-          for (let i = 0; i < d.length; i++) if (!checksum && (word + d[i]).includes(syllable_word)) checksum = true;
-
-          if (!checksum) {
-            syllable_letters = [_l.l];
-
-            n.push(_l.v);
-          }
-        } else {
-          syllable_letters.push(_l.l);
-        }
-      }
-    });
-
-    if (this.debug) console.log(n);
+    let n = this.getValues(result);
 
     if (n.length >= 2) {
       switch(n[1]) {
@@ -312,59 +214,7 @@ class PepperSpray {
       });
     });
 
-    // Get result for wide
-    let syllable = false;
-    let syllable_letters = [];
-    let n = [];
-
-    result.web.forEach((_l, i) => {
-      let letter_type_group = this.getType(_l.l);
-
-      if (!syllable_letters.length) {
-        syllable = true;
-        syllable_letters.push(_l.l);
-
-        n.push(_l.v);
-      } else {
-        let not_a_syllable_of = (letter) => { !letters_knife[letter]['waits_for'][letter_type_group].includes(_l.l) };
-        let not_a_syllable_after_of = (letter) => { return letters_knife[letter]['waits_for']['_after'][_l.l] === undefined };
-        let syllable_after_of = (letter) => { return letters_knife[letter]['waits_for']['_after'][_l.l] };
-
-        if (syllable_letters.length === 1 && not_a_syllable_of(syllable_letters[0])) {
-          syllable_letters = [_l.l];
-
-          n.push(_l.v);
-        } else if (syllable_letters.length === 2 && not_a_syllable_after_of(syllable_letters[1])) {
-          syllable_letters = [_l.l];
-
-          n.push(_l.v);
-        } else if (syllable_letters.length >= 3 && syllable_after_of(syllable_letters[1])) { //del
-          let a = syllable_letters[0];
-          let b = syllable_letters[1];
-          let c = syllable_letters[2];
-          let d = letters_knife[b]['waits_for']['_after'][c];
-
-          let syllable_word = '';
-          let size = syllable_letters.length;
-          let word = a + b + c;
-          let checksum = false;
-
-          for (let i = 0; i < size; i++) syllable_word += syllable_letters[i];
-
-          for (let i = 0; i < d.length; i++) if (!checksum && (word + d[i]).includes(syllable_word)) checksum = true;
-
-          if (!checksum) {
-            syllable_letters = [_l.l];
-
-            n.push(_l.v);
-          }
-        } else {
-          syllable_letters.push(_l.l);
-        }
-      }
-    });
-
-    if (this.debug) console.log(n);
+    let n = this.getValues(result);
 
     if (n.length >= 2) {
       switch(n[1]) {
@@ -400,59 +250,7 @@ class PepperSpray {
       });
     });
 
-    // Get result for wide
-    let syllable = false;
-    let syllable_letters = [];
-    let n = [];
-
-    result.web.forEach((_l, i) => {
-      let letter_type_group = this.getType(_l.l);
-
-      if (!syllable_letters.length) {
-        syllable = true;
-        syllable_letters.push(_l.l);
-
-        n.push(_l.v);
-      } else {
-        let not_a_syllable_of = (letter) => { !letters_knife[letter]['waits_for'][letter_type_group].includes(_l.l) };
-        let not_a_syllable_after_of = (letter) => { return letters_knife[letter]['waits_for']['_after'][_l.l] === undefined };
-        let syllable_after_of = (letter) => { return letters_knife[letter]['waits_for']['_after'][_l.l] };
-
-        if (syllable_letters.length === 1 && not_a_syllable_of(syllable_letters[0])) {
-          syllable_letters = [_l.l];
-
-          n.push(_l.v);
-        } else if (syllable_letters.length === 2 && not_a_syllable_after_of(syllable_letters[1])) {
-          syllable_letters = [_l.l];
-
-          n.push(_l.v);
-        } else if (syllable_letters.length >= 3 && syllable_after_of(syllable_letters[1])) { //del
-          let a = syllable_letters[0];
-          let b = syllable_letters[1];
-          let c = syllable_letters[2];
-          let d = letters_knife[b]['waits_for']['_after'][c];
-
-          let syllable_word = '';
-          let size = syllable_letters.length;
-          let word = a + b + c;
-          let checksum = false;
-
-          for (let i = 0; i < size; i++) syllable_word += syllable_letters[i];
-
-          for (let i = 0; i < d.length; i++) if (!checksum && (word + d[i]).includes(syllable_word)) checksum = true;
-
-          if (!checksum) {
-            syllable_letters = [_l.l];
-
-            n.push(_l.v);
-          }
-        } else {
-          syllable_letters.push(_l.l);
-        }
-      }
-    });
-
-    if (this.debug) console.log(n);
+    let n = this.getValues(result);
 
     if (n.length >= 2) {
       switch(n[1]) {
@@ -488,59 +286,7 @@ class PepperSpray {
       });
     });
 
-    // Get result for wide
-    let syllable = false;
-    let syllable_letters = [];
-    let n = [];
-
-    result.web.forEach((_l, i) => {
-      let letter_type_group = this.getType(_l.l);
-
-      if (!syllable_letters.length) {
-        syllable = true;
-        syllable_letters.push(_l.l);
-
-        n.push(_l.v);
-      } else {
-        let not_a_syllable_of = (letter) => { !letters_knife[letter]['waits_for'][letter_type_group].includes(_l.l) };
-        let not_a_syllable_after_of = (letter) => { return letters_knife[letter]['waits_for']['_after'][_l.l] === undefined };
-        let syllable_after_of = (letter) => { return letters_knife[letter]['waits_for']['_after'][_l.l] };
-
-        if (syllable_letters.length === 1 && not_a_syllable_of(syllable_letters[0])) {
-          syllable_letters = [_l.l];
-
-          n.push(_l.v);
-        } else if (syllable_letters.length === 2 && not_a_syllable_after_of(syllable_letters[1])) {
-          syllable_letters = [_l.l];
-
-          n.push(_l.v);
-        } else if (syllable_letters.length >= 3 && syllable_after_of(syllable_letters[1])) { //del
-          let a = syllable_letters[0];
-          let b = syllable_letters[1];
-          let c = syllable_letters[2];
-          let d = letters_knife[b]['waits_for']['_after'][c];
-
-          let syllable_word = '';
-          let size = syllable_letters.length;
-          let word = a + b + c;
-          let checksum = false;
-
-          for (let i = 0; i < size; i++) syllable_word += syllable_letters[i];
-
-          for (let i = 0; i < d.length; i++) if (!checksum && (word + d[i]).includes(syllable_word)) checksum = true;
-
-          if (!checksum) {
-            syllable_letters = [_l.l];
-
-            n.push(_l.v);
-          }
-        } else {
-          syllable_letters.push(_l.l);
-        }
-      }
-    });
-
-    if (this.debug) console.log(n);
+    let n = this.getValues(result);
 
     if (n.length >= 2) {
       switch(n[1]) {
@@ -568,59 +314,7 @@ class PepperSpray {
       });
     });
 
-    // Get result for wide
-    let syllable = false;
-    let syllable_letters = [];
-    let n = [];
-
-    result.web.forEach((_l, i) => {
-      let letter_type_group = this.getType(_l.l);
-
-      if (!syllable_letters.length) {
-        syllable = true;
-        syllable_letters.push(_l.l);
-
-        n.push(_l.v);
-      } else {
-        let not_a_syllable_of = (letter) => { !letters_knife[letter]['waits_for'][letter_type_group].includes(_l.l) };
-        let not_a_syllable_after_of = (letter) => { return letters_knife[letter]['waits_for']['_after'][_l.l] === undefined };
-        let syllable_after_of = (letter) => { return letters_knife[letter]['waits_for']['_after'][_l.l] };
-
-        if (syllable_letters.length === 1 && not_a_syllable_of(syllable_letters[0])) {
-          syllable_letters = [_l.l];
-
-          n.push(_l.v);
-        } else if (syllable_letters.length === 2 && not_a_syllable_after_of(syllable_letters[1])) {
-          syllable_letters = [_l.l];
-
-          n.push(_l.v);
-        } else if (syllable_letters.length >= 3 && syllable_after_of(syllable_letters[1])) { //del
-          let a = syllable_letters[0];
-          let b = syllable_letters[1];
-          let c = syllable_letters[2];
-          let d = letters_knife[b]['waits_for']['_after'][c];
-
-          let syllable_word = '';
-          let size = syllable_letters.length;
-          let word = a + b + c;
-          let checksum = false;
-
-          for (let i = 0; i < size; i++) syllable_word += syllable_letters[i];
-
-          for (let i = 0; i < d.length; i++) if (!checksum && (word + d[i]).includes(syllable_word)) checksum = true;
-
-          if (!checksum) {
-            syllable_letters = [_l.l];
-
-            n.push(_l.v);
-          }
-        } else {
-          syllable_letters.push(_l.l);
-        }
-      }
-    });
-
-    if (this.debug) console.log(n);
+    let n = this.getValues(result);
 
     if (n.length >= 2) {
       switch(n[1]) {
@@ -660,59 +354,7 @@ class PepperSpray {
       });
     });
 
-    // Get result for wide
-    let syllable = false;
-    let syllable_letters = [];
-    let n = [];
-
-    result.web.forEach((_l, i) => {
-      let letter_type_group = this.getType(_l.l);
-
-      if (!syllable_letters.length) {
-        syllable = true;
-        syllable_letters.push(_l.l);
-
-        n.push(_l.v);
-      } else {
-        let not_a_syllable_of = (letter) => { !letters_knife[letter]['waits_for'][letter_type_group].includes(_l.l) };
-        let not_a_syllable_after_of = (letter) => { return letters_knife[letter]['waits_for']['_after'][_l.l] === undefined };
-        let syllable_after_of = (letter) => { return letters_knife[letter]['waits_for']['_after'][_l.l] };
-
-        if (syllable_letters.length === 1 && not_a_syllable_of(syllable_letters[0])) {
-          syllable_letters = [_l.l];
-
-          n.push(_l.v);
-        } else if (syllable_letters.length === 2 && not_a_syllable_after_of(syllable_letters[1])) {
-          syllable_letters = [_l.l];
-
-          n.push(_l.v);
-        } else if (syllable_letters.length >= 3 && syllable_after_of(syllable_letters[1])) { //del
-          let a = syllable_letters[0];
-          let b = syllable_letters[1];
-          let c = syllable_letters[2];
-          let d = letters_knife[b]['waits_for']['_after'][c];
-
-          let syllable_word = '';
-          let size = syllable_letters.length;
-          let word = a + b + c;
-          let checksum = false;
-
-          for (let i = 0; i < size; i++) syllable_word += syllable_letters[i];
-
-          for (let i = 0; i < d.length; i++) if (!checksum && (word + d[i]).includes(syllable_word)) checksum = true;
-
-          if (!checksum) {
-            syllable_letters = [_l.l];
-
-            n.push(_l.v);
-          }
-        } else {
-          syllable_letters.push(_l.l);
-        }
-      }
-    });
-
-    if (this.debug) console.log(n);
+    let n = this.getValues(result);
 
     if (n.length >= 2) {
       switch(n[1]) {
@@ -752,59 +394,7 @@ class PepperSpray {
       });
     });
 
-    // Get result for wide
-    let syllable = false;
-    let syllable_letters = [];
-    let n = [];
-
-    result.web.forEach((_l, i) => {
-      let letter_type_group = this.getType(_l.l);
-
-      if (!syllable_letters.length) {
-        syllable = true;
-        syllable_letters.push(_l.l);
-
-        n.push(_l.v);
-      } else {
-        let not_a_syllable_of = (letter) => { !letters_knife[letter]['waits_for'][letter_type_group].includes(_l.l) };
-        let not_a_syllable_after_of = (letter) => { return letters_knife[letter]['waits_for']['_after'][_l.l] === undefined };
-        let syllable_after_of = (letter) => { return letters_knife[letter]['waits_for']['_after'][_l.l] };
-
-        if (syllable_letters.length === 1 && not_a_syllable_of(syllable_letters[0])) {
-          syllable_letters = [_l.l];
-
-          n.push(_l.v);
-        } else if (syllable_letters.length === 2 && not_a_syllable_after_of(syllable_letters[1])) {
-          syllable_letters = [_l.l];
-
-          n.push(_l.v);
-        } else if (syllable_letters.length >= 3 && syllable_after_of(syllable_letters[1])) { //del
-          let a = syllable_letters[0];
-          let b = syllable_letters[1];
-          let c = syllable_letters[2];
-          let d = letters_knife[b]['waits_for']['_after'][c];
-
-          let syllable_word = '';
-          let size = syllable_letters.length;
-          let word = a + b + c;
-          let checksum = false;
-
-          for (let i = 0; i < size; i++) syllable_word += syllable_letters[i];
-
-          for (let i = 0; i < d.length; i++) if (!checksum && (word + d[i]).includes(syllable_word)) checksum = true;
-
-          if (!checksum) {
-            syllable_letters = [_l.l];
-
-            n.push(_l.v);
-          }
-        } else {
-          syllable_letters.push(_l.l);
-        }
-      }
-    });
-
-    if (this.debug) console.log(n);
+    let n = this.getValues(result);
 
     if (n.length >= 2) {
       switch(n[1]) {
@@ -826,7 +416,43 @@ class PepperSpray {
   }
 
   j(word, w) {
+    let _letters = [];
+    let result = {
+      word,
+      wide: cangjie['field'],
+      web: []
+    };
     
+    w.forEach((l) => {
+      result.web.push({
+        l,
+        v: letters[l]
+      });
+    });
+
+    let n = this.getValues(result);
+
+    if (n.length >= 2) {
+      switch(n[1]) {
+        case 'value':
+          result.wide = cangjie['mountain'];
+          break;
+
+        case 'function':
+          result.wide = cangjie['middle'];
+          break;
+
+        case 'behavior':
+          result.wide = cangjie['day'];
+          break;
+
+        case 'result':
+          result.wide = cangjie['value'];
+          break;
+      }
+    }
+
+    this.result.push(result);
   }
 
   k(word, w) {
